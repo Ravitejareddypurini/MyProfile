13319ded40af0e2346adb49811770ea50c5ff7e3
Ravi Teja Reddy <reddy@Ravis-MacBook-Pro.local>
2026-04-14 09:59:25 +0100

    recent1

--- src/App.js
+++ src/App.js
@@ -123,7 +123,7 @@ function App() {
         </div>
         <h2 className='experience-sub-title project-title'>E-Commerce</h2>
         <div class="btn-container">
-          <a href="https://github.com/Ravitejareddypurini/todo/tree/master/todolist" class="btn-color-2 project-btn">Github</a>
+          <a href="https://github.com/Ravitejareddypurini/ecommerce" class="btn-color-2 project-btn">Github</a>
           {/* <button class="btn-color-2 project-btn">Demo</button> */}
         </div>
        </div>
